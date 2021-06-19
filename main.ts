9e162c53ea3665a2e404911ffd903b3c84083efc
bob <rag700504@hotmail.com>
2021-06-19 03:53:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    y = y + 1
+    OLED12864_I2C.pixel(x, y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    x = x + 1
+    OLED12864_I2C.pixel(x, y, 1)
+})
+let x = 0
+let y = 0
 OLED12864_I2C.init(60)
 OLED12864_I2C.on()
 let counter = 0
+y = 10
+x = 10
 basic.forever(function () {
     basic.showString(convertToText(counter))
     OLED12864_I2C.showString(
@@ -21,6 +33,7 @@ basic.forever(function () {
     counter,
     3
     )
-    basic.pause(100)
+    OLED12864_I2C.draw()
     counter = counter + 1
+    basic.pause(100)
 })
